c2ba50e37d7150bfeb8972575138c9a6e79422d9
hjpark <hjpark@rdv.ai>
2024-02-15 12:41:11 +0900

    initial commit

---
+++ bag_to_image.sh
@@ -0,0 +1,3 @@
+python bag_to_images.py 20240214_sangju_13room/camera_data_angle.bag 20240214_sangju_13room/images /aurora/rgb/image_raw --file_header=angle
+python bag_to_images.py 20240214_sangju_13room/camera_data_close.bag 20240214_sangju_13room/images /aurora/rgb/image_raw --file_header=close
+python bag_to_images.py 20240214_sangju_13room/camera_data_far.bag 20240214_sangju_13room/images /aurora/rgb/image_raw --file_header=far
